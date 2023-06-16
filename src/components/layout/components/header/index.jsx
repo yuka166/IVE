98ccad6753c3890e365e1b12412a194072657beb
yuka166@github <hoangdat1606@gmail.com>
2023-06-17 03:08:36 +0700

    style responsive for menu and member page

--- src/components/layout/components/header/index.jsx
+++ src/components/layout/components/header/index.jsx
@@ -5,10 +5,28 @@ import { Link } from 'react-router-dom';
 import Logo from '../../../../assets/images/logo.svg';
 import './header.css';
 
+function Nav(props) {
+    return (
+        <nav>
+            <Link to='/about'>Giới thiệu</Link>
+            <Link to='/members'>Thành viên</Link>
+            {!props.hideMenu && <Link to='/'><img src={Logo} alt='Ive logo' className='header-logo' /></Link>}
+            <Link to='/discography'>Discography</Link>
+            <Link to='/videos'>Video</Link>
+        </nav>
+    )
+}
+
 function Header() {
     const [hideMenu, setHideMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
     function MenuChanged() {
         window.innerWidth < 821 ? setHideMenu(true) : setHideMenu(false)
+    }
+    function ShowHideMenu() {
+        showMenu ? setShowMenu(false) : setShowMenu(true);
+        const bodyTag = document.body;
+        !showMenu ? bodyTag.style.position = "fixed" : bodyTag.style.position = "static"
     }
     useEffect(() => {
         window.addEventListener('resize', () => {
@@ -19,12 +37,14 @@ function Header() {
 
     return (
         <header>
-            <Link to='/about'>Giới thiệu</Link>
-            <Link to='/members'>Thành viên</Link>
-            <Link to='/'><img src={Logo} alt='Ive logo' className='header-logo' /></Link>
-            <Link to='/discography'>Discography</Link>
-            <Link to='/videos'>Video</Link>
-            {hideMenu && <div><FontAwesomeIcon icon={faBars} /></div>}
+            {hideMenu &&
+                <>
+                    <Link to='/'><img src={Logo} alt='Ive logo' className='header-logo' /></Link>
+                    <button onClick={ShowHideMenu}><FontAwesomeIcon icon={faBars} /></button>
+                    {showMenu && <div><Nav hideMenu={hideMenu} /></div>}
+                </>
+            }
+            {!hideMenu && <Nav hideMenu={hideMenu} />}
         </header>
     )
 }
